2aa2bea98f757d4b0bc339e3208b105d5a99e72a
Emily Betham <emily.betham@bath.edu>
2019-02-15 17:29:56 +0100

    photo uploader works

---
+++ db/migrate/20190215115044_add_photo_column_to_cocktails.rb
@@ -0,0 +1,5 @@
+class AddPhotoColumnToCocktails < ActiveRecord::Migration[5.2]
+  def change
+    add_column :cocktails, :photo, :string
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,4 @@ Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
 
+
